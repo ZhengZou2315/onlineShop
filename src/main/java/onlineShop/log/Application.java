cad01ab1e21266981e44ff6f88d7ce01730483a7
ZhengZou2315 <55365367+ZhengZou2315@users.noreply.github.com>
2020-11-08 16:41:43 -0500

    Initial commit

---
+++ src/main/java/onlineShop/log/Application.java
@@ -0,0 +1,11 @@
+package onlineShop.log;
+
+//import onlineShop.log.ApplicationConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+    }
+}
---
+++ src/main/java/onlineShop/log/ApplicationConfig.java
@@ -0,0 +1,47 @@
+package onlineShop.log;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationConfig {
+
+    @Bean(name = "sessionFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("onlineShop.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        // 只需要修改红色部分, 保留其他内容
+        dataSource.setUrl("jdbc:mysql://laiproject-instance.cnvmxp8o9agt.us-east-2.rds.amazonaws.com:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("Back13606843459");
+
+        return dataSource;
+    }
+
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update"); // use update so db won't be cleared every time tomcat shut down...
+//        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+//        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        return hibernateProperties;
+    }
+}
+
+
